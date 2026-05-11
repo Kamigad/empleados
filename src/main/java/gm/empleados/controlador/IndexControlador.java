0d9a53ced678cb4766b22792c70e2bb9e408984c
Kamigad <camilo971939@gmail.com>
2026-05-11 16:05:36 -0500

    implementacion del caso de agregar empleados con Spring MVC

--- src/main/java/gm/empleados/controlador/IndexControlador.java
+++ src/main/java/gm/empleados/controlador/IndexControlador.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,5 +27,17 @@ public class IndexControlador {
          //COmpartimos el modelo con la vista
          modelo.put("empleados", empleados);
          return "index"; //index.jsp
+     }
+
+     @RequestMapping(value = "/agregar",method = RequestMethod.GET)
+    public String mostrarAgregar(){
+        return "agregar";//agregar.jsp
+     }
+
+     @RequestMapping(value = "/agregar", method = RequestMethod.POST)
+    public String agregar(@ModelAttribute("empleadoForma") Empleado empleado){
+         logger.info("Empleado agregado " + empleado);
+        servicioEmpleado.agregarEmpleado(empleado);
+        return "redirect:/";
      }
 }
--- src/main/webapp/WEB-INF/jsp/comunes/navegacion.jsp
+++ src/main/webapp/WEB-INF/jsp/comunes/navegacion.jsp
@@ -12,7 +12,7 @@
                         <a class="nav-link active" aria-current="page" href="/empleados/">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/empleados/agregar">Agregar</a>
+                        <a class="nav-link" href="/empleados/agregar">Agregar Empleado</a>
                     </li>
                 </ul>
             </div>
